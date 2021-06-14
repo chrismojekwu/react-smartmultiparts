eab13c5cbeade06f8b781f61133b745ee7cbc7d7
Chris <chrismojekwu@gmail.com>
2021-06-14 14:43:41 -0500

    added needed dependencies

--- src/landingpage/index.js
+++ src/landingpage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-if (module.hot) module.hot.accept()
+if (module.hot) module.hot.accept();
 
 ReactDOM.render(
   <App/>,
