b49580971e2d4013617aafc380702b26fe1a0a2d
Chris <chrismojekwu@gmail.com>
2021-06-15 19:54:39 -0500

    uninstalled storybook dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.config');
+const common = require('./webpack.config');
 
  module.exports = merge(common, {
    mode: 'production',
