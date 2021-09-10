8447779139426bce9db14171e136db0028311b58
Chris <chrismojekwu@gmail.com>
2021-09-09 19:41:37 -0500

    changed router

--- src/landingpage/index.js
+++ src/landingpage/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 if (module.hot) module.hot.accept();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App/>
-  </BrowserRouter>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 )
