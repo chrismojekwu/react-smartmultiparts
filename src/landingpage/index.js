7ca24ef73aa0632fac78d3aa26f7378aa902185c
Chris <chrismojekwu@gmail.com>
2022-05-13 11:28:01 -0700

    did some stuff

--- src/landingpage/index.js
+++ src/landingpage/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot }from 'react-dom/client'
 import App from './App'
 import {HashRouter} from 'react-router-dom'
 
 if (module.hot) module.hot.accept();
 
-ReactDOM.render(
-  <HashRouter>
-    <App/>
-  </HashRouter>, document.getElementById('root')
-)
+const container =  document.getElementById('root');
+const root = createRoot(container);
+root.render(<HashRouter><App/></HashRouter>);
