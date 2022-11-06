3b5bbb4f60290b0d7196cf95567af453ffececd2
Chris <chrismojekwu@gmail.com>
2022-11-05 19:44:49 -0500

    fixed getting started

--- src/landingpage/components/GetStarted.js
+++ src/landingpage/components/GetStarted.js
@@ -75,25 +75,25 @@ const GetStarted = () => {
                         <span className="bold">Range</span> - You can generate a range field by using "range" followed by a bracket that specifies min, max, step, & label values separated by an underscore. Optionally you can include left angle bracket to instruct the form to render the label before the input.
                     </span>
                     <span>
-                        <span className="bold">Range</span> - You can generate a value checkbox by using "checkbox" followed by a value surrounded by brackets.
+                        <span className="bold">Value Checkbox</span> - You can generate a value checkbox by using "checkbox" followed by a value surrounded by brackets.
                     </span>
-                    <span>
-                        <span className="bold">Value Checkbox</span> - You can generate a select field by using "select" in a fields array. You must provide a matching array of select objects to correspond with the number of selects you would like to generate. The form will render selects based on their position in the fields array and the order inside of the select objects array.
-                    <details>
-                        <summary>Example</summary>
-                        <div className="code-snippet">
-                            <code>
-                                {select.map((x,i) => {
-                                    return (
-                                        <div key={i}>
-                                            {x}
-                                        </div>
-                                    )
-                                })}
-                            </code>
-                        </div>
-                    </details>
-                    </span>
+                    <div>
+                        <span className="bold">Select</span> - You can generate a select field by using "select" in a fields array. You must provide a matching array of select objects to correspond with the number of selects you would like to generate. The form will render selects based on their position in the fields array and the order inside of the select objects array.
+                        <details>
+                            <summary>Example</summary>
+                            <div className="code-snippet">
+                                <code>
+                                    {select.map((x,i) => {
+                                        return (
+                                            <div key={i}>
+                                                {x}
+                                            </div>
+                                        )
+                                    })}
+                                </code>
+                            </div>
+                        </details>
+                    </div>
                 </div>
             </>
         );
